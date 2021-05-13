0f049b065c13066ee4d8b72df5b751f72a14048a
Rajan CHoudhary <choudharyrajany2@gmail.com>
2021-05-13 11:35:56 +0530

    azure container registry and kubernetes

--- Test.cs
+++ Test.cs
@@ -1,1 +1,6 @@
 //Test.cs
+az acr create --resource-group myResourceGroup --name rajan --sku Basic
+az acr list --resource-group myResourceGroup --query "[].{acrLoginServer:loginServer}" --output json
+az acr login --name sriniiyer
+docker tag mcr.microsoft.com/azuredocs/azure-vote-front:v1 rajanchoudhary.azurecr.io/azure-vote-front:v1
+docker push rajanchoudhary.azurecr.io/azure-vote-front:v1 
